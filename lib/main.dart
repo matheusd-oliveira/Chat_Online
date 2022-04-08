2c7976f3819c24feb33c60f5be3f0792cd6aa9c8
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-04-08 18:11:41 -0300

    try to use firebase dependencies

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:chat_online/home_page.dart';
-import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp();
+
   runApp(
     MaterialApp(
       home: HomePage(),
     ),
   );
-  
   FirebaseFirestore.instance.collection('mensagens').doc('msg1').set(
     {
       'texto': 'olá, tudo bem Matheus?',
