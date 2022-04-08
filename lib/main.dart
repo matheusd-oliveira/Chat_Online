1848608b4b1f1d0328439787123cda62442705ce
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-04-08 17:04:20 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,17 @@
 import 'package:chat_online/home_page.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseConfig.platformOptions);
   runApp(
     MaterialApp(
       home: HomePage(),
     ),
   );
+  Firestore.instance
+      .collection('col')
+      .document('doc')
+      .setData({'texto': 'matheus'});
 }
