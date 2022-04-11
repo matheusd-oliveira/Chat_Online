84e9a5069e769ab76fb692826ddcdf06446454f4
matheusd-oliveira <matheussilvaolveira@gmail.com>
2022-04-11 18:58:31 -0300

    learning about firebase database

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:chat_online/home_page.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -12,12 +14,33 @@ void main() async {
       home: HomePage(),
     ),
   );
-  FirebaseFirestore.instance
-      .collection('mensagens')
-      .doc('msg1')
-      .collection('arquivos')
-      .doc()  
-      .set(
-    {'arqname': 'foto.png'},
-  );
+// Criando dados no banco de dados
+  // FirebaseFirestore.instance
+  //     .collection('mensagens')
+  //     .doc('msg1')
+  //     .collection('arquivos')
+  //     .doc()
+  //     .set(
+  //   {'arqname': 'foto.png'},
+  // );
+
+// lendo dados no banco de dados de um dado
+  // DocumentSnapshot snapshot = await FirebaseFirestore.instance
+  //     .collection('mensagens')
+  //     .doc('B16KLhqc0We8koSkNpXP')
+  //     .get();
+
+// Lendo dados no banco de dados de todos os documentos
+  // QuerySnapshot snapshot2 =
+  //     await FirebaseFirestore.instance.collection('mensagens').get();
+  // snapshot2.docs.forEach((d) {
+  //   d.reference.update({'lido': true});
+  // });
+
+// Saber se os dados foram atualizados automaticamente
+  // await FirebaseFirestore.instance.collection('mensagens').snapshots().listen((dado) {
+  //   dado.docs.forEach((d) {
+  //      debugPrint(d.data as String);
+  //   });
+  // });
 }
